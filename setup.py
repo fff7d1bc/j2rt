ab72c567a8a1911dbdbf11e8edde161925d927d8
KARBOWSKI Piotr <piotr.karbowski@gmail.com>
2020-06-07 22:42:56 +0200

    setup.py: update

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import setup
 from pathlib import Path
 
 
-def get_requirements():
-    return Path('requirements.txt').read_text().splitlines()
-
-
 setup(
     name="j2rt",
-    version='0.0.1',
-    install_requires=get_requirements(),
+    version='0.0.2',
+    description="Jinja2 rendering tool",
+    long_description=Path('README.rst').read_text(),
+    long_description_content_type='text/x-rst',
+    install_requires=Path('requirements.txt').read_text().splitlines(),
     package_dir={'j2rt': 'src/j2rt'},
     packages=['j2rt'],
     entry_points={'console_scripts': ['j2rt = j2rt:main']},
