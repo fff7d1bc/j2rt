e23b576ec994233fe387c369a41fd9fa6955d53a
KARBOWSKI Piotr <piotr.karbowski@gmail.com>
2020-06-08 20:22:36 +0200

    setup.py: update

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
-from setuptools import setup
 from pathlib import Path
+
+from setuptools import setup
+
+from src.j2rt.version import version
 
 
 setup(
     name="j2rt",
-    version='0.0.2',
+    version=version,
     description="Jinja2 rendering tool",
     long_description=Path('README.rst').read_text(),
     long_description_content_type='text/x-rst',
+    author="Piotr Karbowski",
+    license="BSD",
+    url="https://github.com/slashbeast/j2rt",
+    download_url = "https://github.com/slashbeast/j2rt/archive/v{}.tar.gz".format(version),
     install_requires=Path('requirements.txt').read_text().splitlines(),
     package_dir={'j2rt': 'src/j2rt'},
     packages=['j2rt'],
